b87c4b24c6390a3e4805f814afe2dd5116e190ba
kueben96 <kueben95@gmail.com>
2021-12-17 15:12:39 +0100

    41 - add custom error handling for custom authentication

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   late String _token;
@@ -14,15 +15,34 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url =
         "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB9N4Z66yv4SnwRoMgmjAGJsOwjZD1lbYo";
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {'email': email, 'password': password, 'returnSecureToken': true},
-      ),
-    );
-    var responseData = json.decode(response.body);
-    //_token = response;
-    print(responseData);
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {'email': email, 'password': password, 'returnSecureToken': true},
+        ),
+      );
+      // check response data
+      var responseData = json.decode(response.body);
+
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+
+      responseData = responseData as Map<String, dynamic>;
+      print('test print');
+      print(responseData['idToken']);
+      // responseData.forEach((key, value) {
+      //   print(key);
+      //   print(value);
+      // });
+
+      //_token = response;
+      print(responseData);
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(
